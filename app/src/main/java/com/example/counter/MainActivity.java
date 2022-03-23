455e9e1e1b4f70cbe01fdaf8945a884766d2ee37
elfsvs <94927221+elfsvs@users.noreply.github.com>
2022-03-23 18:46:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.counter;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    int minteger = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void increaseInteger(View view) {
+        minteger = minteger + 1;
+        display(minteger);
+    }
+
+    private void display(int number) {
+        TextView displayInteger = (TextView) findViewById(
+                R.id.integer_number);
+        displayInteger.setText("Integer: " + number);
+    }
+}
